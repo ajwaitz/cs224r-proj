aa79ea1b3604d32f6fc3275c75ea57dbbb8b6da8
Alexander Waitz <ajwaitz@gmail.com>
2025-05-30 22:00:13 -0700

    minor changes

--- ttt-lm-pytorch/ttt.py
+++ ttt-lm-pytorch/ttt.py
@@ -596,7 +596,6 @@ class TTTCache:
     def ttt_params_to_dict(self, layer_idx):
         return {name: self.ttt_params_dict[name][layer_idx] for name in self.ttt_params_dict}
 
-
 class TTTBase(nn.Module):
     def __init__(self, config: TTTConfig, layer_idx: Optional[int] = None):
         super().__init__()
@@ -1395,12 +1394,14 @@ class TTTModel(TTTPreTrainedModel):
         config: TTTConfig
     """
 
-    def __init__(self, config: TTTConfig):
+    def __init__(self, config: TTTConfig, max_episode_steps: int):
         super().__init__(config)
         self.padding_idx = config.pad_token_id
         self.vocab_size = config.vocab_size
 
-        self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
+        self.max_episode_steps = max_episode_steps
+
+        self.embed_tokens = nn.Embedding(self.vocab_size, config.hidden_size, self.padding_idx)
         self.layers = nn.ModuleList([Block(config, layer_idx) for layer_idx in range(config.num_hidden_layers)])
         self.norm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
         self.gradient_checkpointing = False
